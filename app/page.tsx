e9b6281638a607de1c83fa467d19ada7eb0b72ef
Josue <96113041+Rowada@users.noreply.github.com>
2024-06-27 10:07:08 +0200

    style: add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Hello </h1>
+    <main className="mx-auto flex min-h-full max-w-4xl flex-col gap-8 p-4">
+      <nav className="flex items-center justify-start">
+        <Image src="/timer-icon.svg" width={50} height={50} alt="App icon" />
+        <h1 className="text-lg">
+          Time<span className="font-semibold text-primary">Master</span>
+        </h1>
+      </nav>
     </main>
   );
 }
