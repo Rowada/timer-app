6bd006f52f0de1bfb7d3b0f9fd194fe38a1e8350
Josue <96113041+Rowada@users.noreply.github.com>
2024-07-03 10:06:37 +0200

    feat: add priority in image

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,13 @@ export default function Home() {
   return (
     <main className="mx-auto flex min-h-full max-w-4xl flex-col gap-8 p-4">
       <nav className="flex items-center justify-start">
-        <Image src="/timer-icon.svg" width={50} height={50} alt="App icon" />
+        <Image
+          src="/timer-icon.svg"
+          width={50}
+          height={50}
+          priority={true}
+          alt="App icon"
+        />
         <h1 className="text-lg">
           Time<span className="font-semibold text-primary">Master</span>
         </h1>
