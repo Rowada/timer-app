693b6bbe9ef0f6d6a99756f1489bad9c7e10642c
Josue <96113041+Rowada@users.noreply.github.com>
2024-07-03 10:07:50 +0200

    feat: update timer form for use miliseconds

--- components/timer/AddTimerForm.tsx
+++ components/timer/AddTimerForm.tsx
@@ -1,57 +1,69 @@
+import { formatTimeValue } from "@/components/timer/formatTimeValue";
 import React, { useState } from "react";
 import { useTimerStore } from "../../app/timer.store";
-import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 export const AddTimerForm: React.FC = () => {
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [time, setTime] = useState({ hrs: 0, mins: 0, secs: 0 });
   const addTimer = useTimerStore((state) => state.addTimer);
 
-  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const { name, value } = e.target;
-  //   setTime((prevTime) => ({
-  //     ...prevTime,
-  //     [name]: Number(value),
-  //   }));
-  // };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTime((prevTime) => ({
+      ...prevTime,
+      [name]: formatTimeValue(value, name === "hrs" ? 23 : 59),
+    }));
+  };
 
   const handleSubmit = () => {
-    const duration = (hours * 3600 + minutes * 60 + seconds) * 1000;
-    addTimer(duration);
-    setHours(0);
-    setMinutes(0);
-    setSeconds(0);
+    const ms = time.hrs * 3600000 + time.mins * 60000 + time.secs * 1000;
+
+    if (ms < 1000) {
+      alert("Please enter a valid time");
+      return;
+    }
+
+    addTimer(ms);
+    setTime({ hrs: 0, mins: 0, secs: 0 });
   };
 
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center justify-center gap-2">
+        <div className="flex items-center justify-between">
+          {["hrs", "mins", "secs"].map((label) => (
+            <p className="text-center font-bold w-full" key={label}>
+              {label}
+            </p>
+          ))}
+        </div>
         <Input
           type="text"
-          value={String(hours).padStart(2, "0")}
+          value={String(time.hrs).padStart(2, "0")}
           name="hrs"
-          onChange={(e) => setHours(Number(e.target.value))}
+          onChange={handleChange}
         />
         <p className="text-lg">:</p>
         <Input
           type="text"
-          value={String(minutes).padStart(2, "0")}
+          value={String(time.mins).padStart(2, "0")}
           name="mins"
-          onChange={(e) => setMinutes(Number(e.target.value))}
+          onChange={handleChange}
         />
         <p className="text-lg">:</p>
         <Input
           type="text"
-          value={String(seconds).padStart(2, "0")}
+          value={String(time.secs).padStart(2, "0")}
           name="secs"
-          onChange={(e) => setSeconds(Number(e.target.value))}
+          onChange={handleChange}
         />
       </div>
-      <Button onClick={handleSubmit} className="btn-success btn">
-        Add Timer
-      </Button>
+      <div className="flex items-end">
+        <Button onClick={handleSubmit} className="btn-success btn">
+          Add Timer
+        </Button>
+      </div>
     </div>
   );
 };
