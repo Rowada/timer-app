5014607b751292016c00824c76e3210c82033e98
Josue <96113041+Rowada@users.noreply.github.com>
2024-07-17 11:05:28 +0200

    style: update timer card

--- components/timer/Timer.tsx
+++ components/timer/Timer.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Timer as TimerType, useTimerStore } from "../../app/timer.store";
 import { Button } from "../ui/button";
-import { Card } from "../ui/card";
+import { Card, CardTitle } from "../ui/card";
 import { Play, Pause, Trash2, Repeat } from "lucide-react";
 import clsx from "clsx";
 import { CircularTimer } from "./CircularTimer";
@@ -16,7 +16,8 @@ export const Timer: React.FC<TimerProps> = ({ timer }) => {
   const toggleTimer = useTimerStore((state) => state.toggleTimer);
 
   return (
-    <Card>
+    <Card className="p-3">
+      <CardTitle className="text-center">NewTimer</CardTitle>
       <div
         className={clsx(
           "relative flex size-[224px] flex-col gap-2 rounded-2xl bg-base-200 p-4",
@@ -44,17 +45,27 @@ export const Timer: React.FC<TimerProps> = ({ timer }) => {
             className="btn rounded-full size-12 p-0"
           >
             {timer.isRunning ? (
-              <Pause fill="currentColor" size={14} />
+              <Pause fill="currentColor" size={20} />
             ) : (
-              <Play fill="currentColor" size={14} />
+              <Play fill="currentColor" size={20} />
             )}
           </Button>
+
+          <Button className="btn btn-danger rounded-full  size-12 p-0">
+            <Repeat fill="currentColor" size={20} />
+          </Button>
+
           <Button
             onClick={() => deleteTimer(timer.id)}
-            className="btn btn-danger rounded-full size-12 p-0"
+            className="btn btn-danger rounded-full text-primary size-12 p-0"
           >
-            <Trash2 color="currentColor" size={14} />
+            <Trash2 fill="primary" size={20} />
           </Button>
+        </div>
+        <div className="flex items-center justify-center gap-2 text-sm text-primary p-3">
+          <span className="rounded-full bg-accent py-1 px-2">00 hours</span>
+          <span className="rounded-full bg-accent py-1 px-2">02 mins</span>
+          <span className="rounded-full bg-accent py-1 px-2">00 secs</span>
         </div>
       </div>
     </Card>
